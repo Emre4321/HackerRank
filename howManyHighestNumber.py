b39cc5e271ab10e6109693f8f6d6963526435d22
Emre Ugur <emreugur2022@gmail.com>
2025-02-09 20:47:43 +0000

    added howManyHighestNumber.py

---
+++ howManyHighestNumber.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    answer = 0
+    Sorted = sorted(candles, reverse= True)
+    highest = Sorted[0]
+    for number in candles:
+        if number == highest:
+            answer += 1
+    return answer
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
